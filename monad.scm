9e9ea33f1261bd17cef772af0a02efe088ebe01d
Dan Leslie <dan@ironoxide.ca>
2012-04-11 12:30:25 -0700

    Switched to explicit renaiming macros. Works as a module now. Added do-using.

--- monad.scm
+++ monad.scm
@@ -1,6 +1,10 @@
 (module 
- monad *
- (import scheme chicken extras)
+ monad
+ (define-monad using do-using monad? monad-tag
+   <id>? <id>-bind <id>-unit <id>-run
+   <maybe>? <maybe>-bind <maybe>-unit <maybe>-run
+   <list>? <list>-bind <list>-unit <list>-run)
+ (import scheme chicken extras srfi-1)
 
  ;; Want:
  ;; (define-monad name return bind)
@@ -20,44 +24,70 @@
    (fprintf out "#~S ~S" (monad-tag m) (force (monad-value m))))
 
  (define-syntax define-monad
-   (syntax-rules ()
-     ((define-monad name unit-function binding-function)
-      (let* ((pred (symbol-append name '?))
-             (bind (symbol-append name '-bind))
-             (unit (symbol-append name '-unit))
-             (run (symbol-append name '-run)))
-        (eval 
-         `(begin
-            (define (,pred m)
-              (and (monad? m)
-                   (eq? (monad-tag m) name)))
-            (define (,unit val)
-              (make-monad name (delay (unit-function val))))
-            (define (,bind m1 f)
-              (make-monad name (delay (binding-function (force (monad-value m1)) f))))
-            (define (,run m)
-              (force (monad-value m)))))))))
+   (lambda (f r c)
+     (##sys#check-syntax 'define-monad f '(_ _ _ . _))
+     (let* ((name (cadr f))
+            (unit-function (caddr f))
+            (bind-function (cadddr f))
+            (pred (symbol-append name '?))
+            (bindf (symbol-append name '-bind))
+            (unit (symbol-append name '-unit))
+            (run (symbol-append name '-run)))
+       `(begin
+          (,(r 'define) (,pred m)
+           (and (monad? m)
+                (eq? (monad-tag m) ',name)))
+          (,(r 'define) (,unit val)
+           (make-monad ',name (delay (,unit-function val))))
+          (,(r 'define) (,bindf m1 f)
+           (,(r 'let) ((a (force (monad-value m1))))
+            (make-monad ',name (delay (,bind-function a f)))))
+          (,(r 'define) (,run m)
+           (force (monad-value m))))
+       )))
 
  (define-syntax using
-   (syntax-rules ()
-     ((using name body)
-      (eval
-       (begin
-         `(let ((>>= ,(symbol-append name '-bind))
-                (return ,(symbol-append name '-unit)))
-            body))))))
+   (lambda (f r c)
+     (##sys#check-syntax 'using f '(_ _ . _))
+     (let* ((name (cadr f))
+            (body (cddr f)))
+       `(,(r 'let) ((>>= ,(symbol-append name '-bind))
+                    (return ,(symbol-append name '-unit)))
+         ,@body))))
+
+ ; (>>= (>>= (first) (second)) (third))
+
+ (define-syntax do-using
+   (lambda (f r c)
+     (##sys#check-syntax 'do-using f '(_ _ . _))
+     (letrec ((name (cadr f))
+              (body (cddr f))
+              (bindf (symbol-append name '-bind))
+              (unitf (symbol-append name '-unit))
+              (bind-next 
+               (lambda (previous-monad remaining)
+                 (let* ((next (car remaining))
+                        (rest (cdr remaining))
+                        (current-monad
+                         (if (procedure? (eval next))
+                             `(,bindf ,previous-monad ,next)
+                             `(,unitf ,next))))
+                   (if (eq? '() rest)
+                       current-monad
+                       (bind-next current-monad rest))))))
+       (bind-next '() body))))
 
  (define-monad
-   'idM
+   <id>
    (lambda (a) a)
    (lambda (a f) (f a)))
 
  (define-monad
-   'maybeM
+   <maybe>
    (lambda (a) a)
    (lambda (a f) (if a (f a) #f)))
 
  (define-monad
-   'listM
+   <list>
    (lambda (a) (list a))
    (lambda (a f) (concatenate! (map! f a)))))
